95953bc656da33a55ddf9a90b2c6ce3c2198d934
Leonardo Fernandez <Crosfex@gmail.com>
2022-08-16 01:39:23 -0300

    Mejoras en el login

--- workspace-inicial/js/login.js
+++ workspace-inicial/js/login.js
@@ -2,10 +2,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const btn = document.getElementById('btn');
     const email = document.getElementById('email');
     const contrasenia = document.getElementById('password');
+    const logo = document.getElementById('logo');
+
+    logo.addEventListener('click', ()=>{
+        window.location.href="index.html";
+    })
 
     btn.addEventListener('click', ()=>{
         if(email.value != '' && contrasenia != ''){
-            window.location.href="index.html";
+            window.location.href="home.html";
         }
     })
 })
